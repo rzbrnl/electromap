1bb68251e88938a167575dce958a7a1a7364d467
Josué Ruiz <rzbrnl@gmail.com>
2026-07-22 14:17:02 -0700

    Delete config.example.js

--- config.example.js
+++
@@ -1,9 +0,0 @@
-// Configuración de API Keys
-// Copia este archivo como config.js y agrega tus credenciales
-// NO subas config.js a GitHub
-
-var CONFIG = {
-  GOOGLE_MAPS_KEY: 'TU_API_KEY_AQUI',
-  SUPABASE_URL: 'https://rsiqfnopokgqazzijgdw.supabase.co',
-  SUPABASE_KEY: 'sb_publishable_yIkMs17dLeDPANlfAKH9PA_b7h8Wb5Z'
-};
